41e1856652145f7c4dde8ca3aa4e383305adeb3a
Niels Rogge <niels.rogge1@gmail.com>
2023-08-02 14:41:02 +0200

    Include fixes

--- dataset2metadata/text_detection/models/__init__.py
+++ dataset2metadata/text_detection/models/__init__.py
@@ -2,6 +2,6 @@ from .pan import PAN
 from .fast import FAST
 from .psenet import PSENet
 from .builder import build_model
-from .post_processing.ccl import ccl_cuda
+from .post_processing import ccl
 
 __all__ = ['PAN', 'PSENet', 'FAST']
