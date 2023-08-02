6fc914e77b853ef97ba1bb7caa362f10f78da7c3
Niels Rogge <niels.rogge1@gmail.com>
2023-08-02 15:26:53 +0200

    Include fixes

--- dataset2metadata/text_detection/models/__init__.py
+++ dataset2metadata/text_detection/models/__init__.py
@@ -1,6 +1,6 @@
-from .pan import PAN
+# from .pan import PAN
 from .fast import FAST
-from .psenet import PSENet
+# from .psenet import PSENet
 from .builder import build_model
 from .post_processing import ccl
 
